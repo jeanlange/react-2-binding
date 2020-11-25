a840c9681aefa251f9429c08cb446f6138650ab8
Jean Lange <jean.f.lange@gmail.com>
2020-11-25 12:00:37 -0500

    two way binding on input box

--- src/App.js
+++ src/App.js
@@ -10,12 +10,30 @@ class App extends React.Component {
       myCoolText: ""
     };
   }
+
+  updateCoolText = (eventThatJustHappened) => {
+    // console.log(eventThatJustHappened);
+    // console.log(eventThatJustHappened.target);
+
+    // get whatever's in that input
+    const value = eventThatJustHappened.target.value;
+    console.log(value);
+
+    // store that into the state
+    this.setState({
+      myCoolText: value
+    });
+  }
+
   render() {
-    console.log('hello!');
+    console.log('I am rendering!!');
     return (
       <div>
         <div>hello world</div>
-        <input value={this.state.myCoolText}></input>
+        <input
+          value={this.state.myCoolText}
+          onChange={this.updateCoolText}
+        />
         <h1>{this.state.myCoolText}</h1>
       </div>
     );
